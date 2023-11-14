e42ad1cb435cd98a89b7f038533330dd4a709e65
Adam Shambrook <adamshambrook@Adams-MacBook-Air.local>
2023-11-14 22:13:27 +0000

    updating webserver role

--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -2,9 +2,10 @@
 # All roles should include the base profile
 
 # This role creates a webserver hosting a customer facing webpage
-
 class role::webserver {
-
   include profile::base
-
+  notify {
+    "Hello! my role is: ${trusted['extensions']['pp_role']} \
+    and I've been running for ${facts['uptime']}":
+  }
 }
