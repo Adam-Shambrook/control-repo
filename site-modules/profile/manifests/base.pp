1e5bf92c66a49f3cda60ef1e29fdb488e90939e7
Adam Shambrook <adamshambrook@Adams-MacBook-Air.local>
2023-11-14 21:59:28 +0000

    adding webserver role

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,7 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
-
+  notify {
+    "Hello! my role is: ${trusted['extensions']['pp_role']} \
+     and I've been running for ${facts['uptime']}":
+  }
 }
--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -1,5 +1,8 @@
 # This role would be made of all the profiles that need to be included to make a webserver work
 # All roles should include the base profile
+
+# This role creates a webserver hosting a customer facing webpage
+
 class role::webserver {
 
   include profile::base
