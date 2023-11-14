2e8ffc5a674274e6d57f4b109bbad82b042a05db
Adam Shambrook <adamshambrook@Adams-MacBook-Air.local>
2023-11-14 22:24:50 +0000

    adding a base profile to roles

--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -4,8 +4,4 @@
 # This role creates a webserver hosting a customer facing webpage
 class role::webserver {
   include profile::base
-  notify {
-    "Hello! my role is: ${trusted['extensions']['pp_role']} \
-    and I've been running for ${facts['uptime']}":
-  }
 }
