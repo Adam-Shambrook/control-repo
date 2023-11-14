5bdfb5a243fce9becddd43ceb027b333b154a896
Adam Shambrook <adamshambrook@Adams-MacBook-Air.local>
2023-11-14 22:29:37 +0000

    adding a base profile to roles

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,7 +1,12 @@
-# The base profile should include component modules that will be on all nodes
+# site-modules/profile/manifests/base.pp
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::base
 class profile::base {
-  notify {
-    "Hello! my role is: ${trusted['extensions']['pp_role']} \
-     and I've been running for ${facts['uptime']}":
+  class { 'motd':
+    content => "Hello! You are in a Datadog ${trusted['extensions']['pp_role']} node.\n"
   }
 }
