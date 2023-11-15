edecf396eeba990bfff4f5cdc6c1f40c1c111d7e
Adam Shambrook <adamshambrook@Adams-MacBook-Air.local>
2023-11-15 01:07:41 +0000

    new motd

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,12 +1,34 @@
-# site-modules/profile/manifests/base.pp
-# @summary A short summary of the purpose of this class
-#
-# A description of what this class does
-#
-# @example
-#   include profile::base
-class profile::base {
-  class { 'motd':
-    content => "Hello! You are in a Datadog ${trusted['extensions']['pp_role']} node.\n"
+  $motd = @("MOTD"/L)
+    ===========================================================
+
+          Welcome to ${::hostname}
+
+    Access  to  and  use of this server is  restricted to those
+    activities expressly permitted by the system administration
+    staff. If you are not sure if it is allowed, then DO NOT DO IT.
+
+    ===========================================================
+
+    The operating system is: ${::operatingsystem}
+            The domain is: ${::domain}
+
+    | MOTD
+
+
+  # Check if we have a hiera override for the MOTD, otherwise use the default
+  $message = lookup('motd', String, 'first', $motd)
+
+  class { '::motd':
+    content => $message,
   }
+
+  if !defined(File['/etc/issue']){
+
+    file { '/etc/issue':
+      ensure  => file,
+      content => $message,
+    }
+
+  }
+
 }
---
+++ site-modules/profile/manifests/base_old.pp
@@ -0,0 +1,12 @@
+# site-modules/profile/manifests/base.pp
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::base
+class profile::base {
+  class { 'motd':
+    content => "Hello! You are in a Datadog ${trusted['extensions']['pp_role']} node.\n"
+  }
+}
