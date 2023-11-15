de59b1d253600193815bda8c050b7ed1cd993ef1
Adam Shambrook <adamshambrook@Adams-MacBook-Air.local>
2023-11-15 11:09:11 +0000

    reverting MOTD

--- site-modules/profile/manifests/apache.pp
+++ site-modules/profile/manifests/apache.pp
@@ -8,3 +8,5 @@ class profile::apache {
     docroot => $docroot,
   }
 }
+
+
--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,35 +1,12 @@
-class profile::platform::baseline::linux::motd {
-  $motd = @("MOTD"/L)
-    ===========================================================
-
-          Welcome to ${::hostname}
-
-    Access  to  and  use of this server is  restricted to those
-    activities expressly permitted by the system administration
-    staff. If you are not sure if it is allowed, then DO NOT DO IT.
-
-    ===========================================================
-
-    The operating system is: ${::operatingsystem}
-            The domain is: ${::domain}
-
-    | MOTD
-
-
-  # Check if we have a hiera override for the MOTD, otherwise use the default
-  $message = lookup('motd', String, 'first', $motd)
-
-  class { '::motd':
-    content => $message,
+# site-modules/profile/manifests/base.pp
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::base
+class profile::base {
+  class { 'motd':
+    content => "\n\nHello! You are in a Datadog ${trusted['extensions']['pp_role']} node.\n\n"
   }
-
-  if !defined(File['/etc/issue']){
-
-    file { '/etc/issue':
-      ensure  => file,
-      content => $message,
-    }
-
-  }
-
 }
