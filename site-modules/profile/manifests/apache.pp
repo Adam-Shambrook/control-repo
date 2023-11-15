3253e32cee25c39b0fe156e68538b7cf9aa3e951
Adam Shambrook <adamshambrook@Adams-MacBook-Air.local>
2023-11-15 00:43:51 +0000

    updating apache

--- site-modules/profile/manifests/apache.pp
+++ site-modules/profile/manifests/apache.pp
@@ -1,7 +1,7 @@
 # apache profile: site-modules/profile/manifests/apache.pp
 class profile::apache {
   $port    = 80
-  $docroot = '/var/www'
+  $docroot = '/var/www/welcome.html'
   include apache
   apache::vhost { 'vhost.example.com':
     port    => $port,
